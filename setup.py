e904b4ddd8dbcc25744b2ae2d873fb269245f273
Fumio Matsuda <39853206+fumiomatsuda@users.noreply.github.com>
2022-03-10 11:48:44 +0900

    0.6.1 bug fix

--- setup.py
+++ setup.py
@@ -21,7 +21,8 @@ with open('LICENSE') as f:
 setup(
     name='mfapy',
     version='0.6.1',
-    #package_dir = {'': pkgdir},
+    packages=find_packages(exclude=('tests', 'docs')),
+    package_dir = {'mfapy': ""},
     py_modules =  ["mfapy", "metabolicmodel", "carbonsource", "mfapyio", "mdv", "optimize"],
     description = "Parallel and distributed programming for Python",
     platforms = ["Windows", "Linux", "Unix"],
@@ -31,7 +32,7 @@ setup(
     install_requires=load_requires_from_file('requirements.txt'),
     url='https://github.com/fumiomatsuda/mfapy',
     license=license,
-    packages=find_packages(exclude=('tests', 'docs')),
+
     test_suite='tests'
 )
 
